8891de65a529661e6df5e35b3897866017d83847
3thn-726 <not3thn@gmail.com>
2025-03-24 10:17:23 -0400

    comiy

--- flashcard.py
+++ flashcard.py
@@ -1,2 +1,17 @@
-class teacher:
-    def __init__ (self,)
+import json
+
+class flashcards:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def display(self):
+        print(self.question, self.answer)
+
+techer_or_student = input("techer or studnet mode(t or s):")
+if techer_or_student == "t":
+    class student:
+elif techer_or_student == "s":
+    class teacher:
+        
+else: print("Error")
