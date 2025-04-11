0a46c4509508f228b5770c3e6da8fc7c27afc6ef
3thn-726 <not3thn@gmail.com>
2025-04-11 10:09:10 -0400

    .

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
 import json
 
-class Car:
-    def __init__(self, make, model, year, image=None):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.image = image
-    
-    def display_info(self):
-        return f"{self.year} {self.make} {self.model}"
-    
-    def to_dict(self):
-        return {"make": self.make, "model": self.model, "year": self.year, "image": self.image}
+class flash_cards:
+    def __init__(self, flashcard, answer):
+        self.flashcard = flashcard
+        self.answer = answer
+        flashcard = input("What is your question kid? Type question:")
+        answer = input("What is your answer to this question big guy? Type answer:")
